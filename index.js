6fb32eeba54ce66e178868321fb7e6f338242935
Marc Jermaine Pontiveros <marcjermaine.pontiveros@sun-asterisk.com>
2022-10-24 01:27:50 +0800

    Initial

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
-
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
+server.listen(3000, function () {
+  console.log('Server listening at port %d', 3000);
 });
 
-io.on('connection', function(socket) {
-    socket.on('chat.message', function(message) {
-        io.emit('chat.message', message);
-    });
+// app.get('/', function (req, res) {
+//   return res.send('Hello World!');
+// });
 
-    socket.on('disconnect', function() {
-        io.emit('chat.message', 'User has disconnected.');
-    });
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    io.emit('message', msg);
+  });
 });
